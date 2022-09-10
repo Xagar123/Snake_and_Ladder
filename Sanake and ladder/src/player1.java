9b274a2826242a4c7414bfa360abd8990d216d5b
Xagar123 <sagarcoc2000@gmail.com>
2022-09-11 00:25:16 +0530

    PlayerA roll die and get a number

--- Sanake and ladder/src/player1.java
+++ Sanake and ladder/src/player1.java
@@ -3,8 +3,10 @@ public class player1 {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder Game");
         System.out.println("Both the player is at zero position");
+        int playerA = 0;
+        int playerB = 0;
 
-//        int DieRoll = (int) Math.floor(Math.random() * 10) % 7;
-//        System.out.println("Player A roll die " + "he move " + DieRoll+" Steps");
+        int DieRoll = (int) Math.floor(Math.random() * 10) % 7;
+        System.out.println("Player A roll die " + "he move " + DieRoll+" Steps");
     }
 }
